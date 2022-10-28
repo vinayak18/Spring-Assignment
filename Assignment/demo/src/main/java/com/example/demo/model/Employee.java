d496600e537cdbe1bb3dbb487ab88ada4065795b
vinayak18 <vinayaksaraf1999@gmail.com>
2022-10-28 12:27:40 +0530

    Initial Commit

---
+++ Assignment/demo/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	private String empId;
+	private String empName;
+	private String designation;
+	private String location;
+}
